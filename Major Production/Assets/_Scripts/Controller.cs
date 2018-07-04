3d8f6cde91de4a830da73688ec69e9f44290dc88
Georgia Shepherd <fruitbat_08@hotmail.com>
2018-07-04 12:34:32 +0930

    Added scene to load random encounters to (currently empty), working on making random encounters occur "randomly" at intervals

--- Major Production/Assets/_Scripts/Controller.cs
+++ Major Production/Assets/_Scripts/Controller.cs
@@ -4,43 +4,40 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour {
 	public float speed;
+	public GameObject randCount;
+	public float EncounterTimer; 
 
 	CharacterController characterController;
+	EncounterController encounters;
 	Animator anim;
 	Vector3 moveDir;
 
+
 	// Use this for initialization
 	void Start () {
+
 		characterController = GetComponent<CharacterController>();
 		anim = GetComponent<Animator>();
 		moveDir = Vector3.zero;
 
-		anim.SetBool("isWalking", false);
+		encounters = randCount.GetComponent<EncounterController>();
 	}
 	
-	// Update is called once per frame
+	//currently no isGrounded check
 	void FixedUpdate () {
-		//if(characterController.isGrounded){
-
 		//basic blend tree stuff
 		float x = Input.GetAxis("Horizontal");
 		float y = Input.GetAxis("Vertical");
+		
 		Move(x, y);
-
-		//movement code
-		//moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		//	moveDir = transform.TransformDirection(moveDir);
-		//	//moveDir *= speed;
-
-		//	characterController.Move(moveDir * Time.deltaTime);
-		//	Debug.Log("moving chara");
-		//}
 	}
 
 	//does animation movement 
 	private void Move(float x, float y){
 		anim.SetFloat("velX", x);
 		anim.SetFloat("velY", y);
-
+		if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
+			encounters.RandomEncounter();
+		}
 	}
 }
---
+++ Major Production/Assets/_Scripts/EncounterController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncounterController : MonoBehaviour {
+	public float Probability;
+	float mod;
+	float nextTimeEncounter;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	public void RandomEncounter () {
+
+		//currently doesn't work
+		float rand = Random.Range(0, 100);
+		nextTimeEncounter = Time.time + rand;
+		Debug.Log("Next Time: " + nextTimeEncounter);
+		Debug.Log("Time: " + Time.time);
+		if(Time.time > nextTimeEncounter) {
+			Debug.Log("Random Encounter!");
+			Debug.Log("Random: " + rand);
+
+		}
+		//if(CurveRandomNumber(rand, curve) >= Probability) {
+		//	Debug.Log("Random Encounter");
+		//	Debug.Log(rand);
+		//}
+
+	}
+
+	public float CurveRandomNumber(float rand, AnimationCurve curve){
+		return curve.Evaluate(rand);
+	}
+}

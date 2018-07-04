b52caa25c69ed3ae46f483340dae4753c54956d1
Georgia Shepherd <fruitbat_08@hotmail.com>
2018-07-04 10:46:26 +0930

    First project push, currently has partly done overworld test scene (character movement with animations + camera follow)

---
+++ Major Production/Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform player;
+
+	Quaternion cameraAngle;
+	Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+		cameraAngle = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player != null) {
+			transform.position = new Vector3(player.position.x + position.x, player.position.y + position.y, player.position.z + position.z);
+		}
+	}
+}
---
+++ Major Production/Assets/_Scripts/Controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+	public float speed;
+
+	CharacterController characterController;
+	Animator anim;
+	Vector3 moveDir;
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController>();
+		anim = GetComponent<Animator>();
+		moveDir = Vector3.zero;
+
+		anim.SetBool("isWalking", false);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//if(characterController.isGrounded){
+
+		//basic blend tree stuff
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+		Move(x, y);
+
+		//movement code
+		//moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		//	moveDir = transform.TransformDirection(moveDir);
+		//	//moveDir *= speed;
+
+		//	characterController.Move(moveDir * Time.deltaTime);
+		//	Debug.Log("moving chara");
+		//}
+	}
+
+	//does animation movement 
+	private void Move(float x, float y){
+		anim.SetFloat("velX", x);
+		anim.SetFloat("velY", y);
+
+	}
+}

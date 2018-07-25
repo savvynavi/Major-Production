7c9961a3f9f7b9e0184e7aef3393efa07bd39295
Andrew Spencer <thrawn369@gmail.com>
2018-07-25 11:40:47 +0930

    Created SceneLoader class to load and unload battle scene

---
+++ Major Production/Assets/_Scripts/Scene Management/SceneLoader.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// TODO make more general: use a stack structure to keep track of scenes instead of assuming overword->dungeon->battle
+
+public class SceneLoader : MonoBehaviour {
+
+    Scene dungeonScene;
+    Scene battleScene;
+
+	// Use this for initialization
+	void Start () {
+        // HACK for testing, later dungeonScene will be loaded on reaching dungeon
+        dungeonScene = SceneManager.GetActiveScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Activates or deactivates root objects in scene not tagged "Don't Suspend"
+    void SetSceneObjectActive(Scene scene, bool value)
+    {
+        int rootCount = scene.rootCount;
+        GameObject[] rootObjects = scene.GetRootGameObjects();
+        foreach(GameObject o in rootObjects)
+        {
+            if(!o.CompareTag("Don't Suspend"))
+            {
+                o.SetActive(value);
+            }
+        }
+    }
+
+    public void LoadBattle(string sceneName)
+    {
+        StartCoroutine(AsyncBattleLoad(sceneName));
+    }
+
+    public void EndBattle()
+    {
+        SetSceneObjectActive(dungeonScene, true);
+        SceneManager.SetActiveScene(dungeonScene);
+        SceneManager.UnloadSceneAsync(dungeonScene);
+    }
+
+    IEnumerator AsyncBattleLoad(string sceneName)
+    {
+        AsyncOperation loadOp = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        // TODO do battle loading effects
+        yield return new WaitUntil(() => { return loadOp.isDone; });
+        // Deactivate objects in dungeon scene
+        SetSceneObjectActive(dungeonScene, false);
+        // Get Battle scene and set as active scene
+        battleScene = SceneManager.GetSceneByName(sceneName);
+        SceneManager.SetActiveScene(battleScene);
+    }
+}

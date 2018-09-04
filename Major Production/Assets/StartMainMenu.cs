5755e499b74c00462575cc02dba6007e32a0d23f
AIE\s161878 <matthew.anthony.connole@gmail.com>
2018-09-04 15:34:29 +0930

    I have added a main menu still a work in progress

---
+++ Major Production/Assets/StartMainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartMainMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Tutorial_greybox");
+    }
+
+}

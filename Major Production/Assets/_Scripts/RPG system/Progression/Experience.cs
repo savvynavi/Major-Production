e6834c9bf9d12a5515913479a955dad02013a6f5
Andrew Spencer <thrawn369@gmail.com>
2018-09-12 12:12:16 +0930

    Created Experience component and Level class. Added level stat changes to bard

---
+++ Major Production/Assets/_Scripts/RPG system/Progression/Experience.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPGsys;
+
+namespace RPG.XP
+{
+	[Serializable]
+	public class StatFloatDict : Dialogue.SerializedDictionary<RPGStats.Stats, float> { };
+
+	[Serializable]
+	public class Level
+	{
+		[SerializeField] StatFloatDict StatChanges;
+		public Dictionary<RPGStats.Stats, float> StatChangeDict;
+
+		public void Init()
+		{
+			StatChangeDict = StatChanges.ToDictionary();
+		}
+	}
+
+	[RequireComponent(typeof(Character))]
+	public class Experience : MonoBehaviour
+	{
+
+		[SerializeField] List<Level> levels;
+
+		// Use this for initialization
+		void Awake()
+		{
+			foreach(Level l in levels)
+			{
+				l.Init();
+			}
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}

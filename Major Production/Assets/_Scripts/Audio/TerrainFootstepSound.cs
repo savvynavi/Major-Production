033e12bf6767945ac6a7c09f2c5d804c22523095
Andrew Spencer <thrawn369@gmail.com>
2018-10-10 00:38:35 +1030

    Created FootstepSounds classes

---
+++ Major Production/Assets/_Scripts/Audio/TerrainFootstepSound.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Audio
+{
+    [RequireComponent(typeof(Terrain))]
+    public class TerrainFootstepSound : FootstepSounds
+    {
+        [SerializeField]
+        List<AudioClip> Footsteps;
+
+        Terrain terrain;
+
+        private void Start()
+        {
+            terrain = GetComponent<Terrain>();
+        }
+
+        public override AudioClip GetFootstep(GameObject other)
+        {
+            int terrainIndex = terrain.GetMainTexture(other.transform.position);
+            return Footsteps[terrainIndex];
+            // TODO return default if out of range?
+        }
+    }
+}

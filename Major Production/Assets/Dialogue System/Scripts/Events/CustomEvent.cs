1f47e06a947b16388a95eecd8492f2082705e73b
Andrew Spencer <thrawn369@gmail.com>
2018-10-30 10:46:37 +1030

    Setting up Barricade object in Greybox Test

--- Major Production/Assets/Dialogue System/Scripts/Events/CustomEvent.cs
+++ Major Production/Assets/Dialogue System/Scripts/Events/CustomEvent.cs
@@ -9,16 +9,41 @@ namespace Dialogue
 	{
 		public override string Describe(string target, string parameters)
 		{
-			return "Calls event '" + target + "' on dialogue manager."; 
+			System.Text.StringBuilder description = new System.Text.StringBuilder();
+			description.AppendFormat("Calls event {0}", parameters);
+			if(!string.IsNullOrEmpty(target))
+			{
+				description.AppendFormat(" on actor '{0}'", target);
+			}
+			return "Calls event '" + parameters + "' on dialogue manager."; 
 		}
 
 		public override void Execute(DialogueManager manager, string target, string parameters)
 		{
 			UnityEvent customEvent;
-			if(manager.customEvents.TryGetValue(target, out customEvent))
+			if (string.IsNullOrEmpty(target))
 			{
-				customEvent.Invoke();
+				if (manager.customEvents.TryGetValue(parameters, out customEvent))
+				{
+					customEvent.Invoke();
+				}
+				else
+				{
+					Debug.LogWarning("Event '" + parameters + "; not found in dialogue manager");
+				}
+			} else
+			{
+				DialogueActor actor;
+				if(manager.actors.TryGetValue(target, out actor))
+				{
+					
+				} else
+				{
+					Debug.LogWarning("Actor '" + target + "' not found in dialogue manager");
+				}
 			}
+
+
 		}
 	}
 }
---
+++ Major Production/Assets/_Scripts/Interactables/Barricade.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dialogue;
+
+namespace RPG{
+	[RequireComponent(typeof(DialogueActor))]
+	public class Barricade : Interactable {
+
+		DialogueManager dialogueManager;
+		public Conversation conversation;
+		public DialogueActor actor;
+		public DialogueActor guardActor;
+		// TODO persistent trigger to store being dropped
+
+		// Use this for initialization
+		void Start () {
+			dialogueManager = FindObjectOfType<DialogueManager>();
+			actor = GetComponent<DialogueActor>();
+			//TODO decide whether to have DropBarricade done here or in editor
+		}
+	
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		public override void Interact(InteractionUser user)
+		{
+			// Assign Speakers to my dialogue actors
+			dialogueManager.actors["Barricade"] = actor;
+			dialogueManager.actors["BarricadeGuard"] = guardActor;
+			dialogueManager.StartConversation(conversation);
+		}
+
+		public override void Hilight()
+		{
+			//TODO
+		}
+
+		public override void Unhilight()
+		{
+			//TODO
+		}
+
+		public void DropBarricade()
+		{
+			// TODO
+			Debug.Log("Dropped barricade");
+		}
+	}
+}

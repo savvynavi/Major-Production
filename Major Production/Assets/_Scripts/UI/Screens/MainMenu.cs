30043858fb319de32d971cb028f3f4f2b0867747
Andrew Spencer <thrawn369@gmail.com>
2018-11-13 16:43:15 +1030

    Added Skip To button letting other saves be started at

--- Major Production/Assets/_Scripts/UI/Screens/MainMenu.cs
+++ Major Production/Assets/_Scripts/UI/Screens/MainMenu.cs
@@ -10,10 +10,32 @@ namespace RPG.UI
 	{
 
 		public Dropdown sceneSelect;
+		public Dropdown skipSelect;
+		public Button skipButton;
+		public Dialogue.StringTextAssetDict savesDict;
+		public Dictionary<string, TextAsset> skipSaves;
 		[SerializeField] Button ContinueButton;
 
 		private void Start()
 		{
+			// set up skip ui
+			skipSaves = savesDict.ToDictionary();
+			skipSelect.ClearOptions();
+			List<Dropdown.OptionData> skipOptions = new List<Dropdown.OptionData>();
+			foreach(KeyValuePair<string,TextAsset> skipEntry in skipSaves)
+			{
+				skipOptions.Add(new Dropdown.OptionData(skipEntry.Key));
+			}
+			skipSelect.AddOptions(skipOptions);
+			if(skipSelect.options.Count > 0)
+			{
+				skipSelect.value = 0;
+			}
+			else
+			{
+				skipSelect.gameObject.SetActive(false);
+				skipButton.gameObject.SetActive(false);
+			}
 			//HACK should probably have a SaveManager class knowing what last savefile was
 			if(File.Exists(Application.persistentDataPath + "/savegame.json"))
 			{
@@ -44,6 +66,12 @@ namespace RPG.UI
 		public void Continue()
 		{
 			GameController.Instance.LoadGame();
+		}
+
+		public void StartSkip()
+		{
+			TextAsset saveFile = skipSaves[skipSelect.options[skipSelect.value].text];
+			GameController.Instance.LoadGame(saveFile);
 		}
 	}
 }

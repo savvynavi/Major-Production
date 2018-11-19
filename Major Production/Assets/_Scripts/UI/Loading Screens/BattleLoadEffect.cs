5d4d3cfc3005807099db33421cc51f9bb994c051
Andrew Spencer <thrawn369@gmail.com>
2018-11-19 11:23:33 +1030

    Created abstract class for battle loading effects

---
+++ Major Production/Assets/_Scripts/UI/Loading Screens/BattleLoadEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI
+{
+	public abstract class BattleLoadEffect : MonoBehaviour
+	{
+		// TODO figure out what this needs to know
+		public abstract void BeginBattleLoad();
+
+		public abstract void UpdateLoadProgress(float progress);
+
+		public abstract void BattleSceneReady();
+
+		public abstract void FinishBattleLoad();
+
+		public bool IsActivationAllowed { get; protected set; }
+
+		// TODO figure out this side of things with BattleManager
+		public abstract void StartBattleOutro();
+
+		public bool IsOutroDone { get; protected set; }
+	} 
+}
